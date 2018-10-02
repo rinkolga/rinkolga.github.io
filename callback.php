d1dbfd90cbdd7bf635d8951998390a04e98dba61
rinkolga <rink.olga0705@gmail.com>
2018-10-02 22:32:04 +0600

    02 10 2018
    обратный звонок

---
+++ callback.php
@@ -0,0 +1,24 @@
+<?
+     
+    //Если поступили данные..
+    if( isset($_POST['name'],$_POST['phone'])){
+         
+        $name = trim( htmlspecialchars( substr($_POST['name'], 20) ) );
+        $phone = trim( htmlspecialchars( substr($_POST['phone'], 20) ) );
+ 
+        if( $name != '' AND $phone != '' ){
+             
+            $email = "rink_olga@mail.ru";
+            $domen = "www.RMservice.tk";
+ 
+            $message = 'Имя: '.$name.'<br>Телефон: '.$phone;
+            $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+            $headers .= "From: ".$domen." <robot@".$domen.">\r\n";
+            $subject = "Обратный звонок";
+            mail($email, $subject, $message, $headers);
+            echo 1;
+ 
+        }else echo 0;
+    }
+ 
+?>
